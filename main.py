2b321e8e43b7cfde64a7863830fa922830b32272
Ishu <87901742+Ishu-Hinata@users.noreply.github.com>
2022-06-25 19:51:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging, os, random
 from pyrogram import filters, Client, __version__ as pyro
 from pyrogram.types import *
-
+import time
 import requests 
 
 # enable logging
@@ -809,6 +809,29 @@ def neko(_, message):
     message.reply_text(
         ke.format(name)
     )
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    for i in range(len(time_list)):
+        time_list[i] = str(time_list[i]) + time_suffix_list[i]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+    return ping_time
 
 @app.on_message(filters.command(["ping"])) 
 async def ping(_, message: Message):
