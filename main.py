7fdc265bebc7bf5645a6652e63c0d5d88e781b73
Ishu <87901742+Ishu-Hinata@users.noreply.github.com>
2022-06-25 07:06:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -158,8 +158,8 @@ async def kiss(_, message):
        r = requests.get(url)
        e = r.json()
        kissme = e["results"][0]["url"]
-       name1 = message.from_user.first_name
-       name2 = message.reply_to_message.from_user.first_name
+       name1 = message.from_user.full_name
+       name2 = message.reply_to_message.from_user.full_name
        await message.reply_to_message.reply_video(kissme, caption="*{} kisses {}*~".format(name1, name2))
        return
     else:
