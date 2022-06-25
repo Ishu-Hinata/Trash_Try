bf32f521e88c70f2e886176435f5a59e82f44435
Ishu <87901742+Ishu-Hinata@users.noreply.github.com>
2022-06-25 20:13:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -810,10 +810,35 @@ def neko(_, message):
         ke.format(name)
     )
 
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    for i in range(len(time_list)):
+        time_list[i] = str(time_list[i]) + time_suffix_list[i]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+    return ping_time
+
 @bot.on_message(filters.command(["ping"])) 
 async def ping(_, message: Message):
+    bot_uptime = int(time.time() - boot)
+    Uptime = get_readable_time(bot_uptime)
     image = random.choice(RANDOM)
-    await message.reply_photo(photo=image, caption=f"💖🤍{pyro}🤍💖")
+    await message.reply_photo(photo=image, caption=f"💖🤍{uptime}🤍💖")
     
 bot.run()
 with bot:
